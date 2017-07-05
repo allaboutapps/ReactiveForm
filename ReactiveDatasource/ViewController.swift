839a84a880443e4aebe8370b20e6e9f733d719ab
Michael Heinzl <michael.heinzl@allaboutapps.at>
2017-07-05 11:31:17 +0200

    Initial Commit

---
+++ ReactiveDatasource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveDatasource
+//
+//  Created by Michael Heinzl on 05.07.17.
+//  Copyright © 2017 aaa - all about apps GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
